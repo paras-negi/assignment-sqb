e5765de3aece626088208417463173cb44e001ba
Paras Negi <paras.negi@prepladder.com>
2022-03-07 09:47:10 +0530

    updating code

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,17 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import icons from "../common/Images";
 import { withContext } from "../context/Context";
 
 const { logo, home_main } = icons;
 
-function Header({ context: { isUserLogin, userInfo } }) {
+function Header({ context: { isUserLogin, userInfo }, isLoginPage }) {
+  const navigate = useNavigate();
+
+  const onClickLoginLogout = () => {
+    navigate("/login");
+  };
+
   return (
     <header className="header bg-dark ">
       <div className="headContainer flex justify-space align-center">
@@ -15,13 +22,17 @@ function Header({ context: { isUserLogin, userInfo } }) {
         </div>
 
         <div className="header-right">
-          <div className="header-right__logo">
-            {isUserLogin ? (
-              <div>{userInfo?.name} </div>
-            ) : (
-              <button className="btn login-btn">Login/Signup</button>
-            )}
-          </div>
+          {!isLoginPage ? (
+            <div className="header-right__logo">
+              {isUserLogin ? (
+                <div>{userInfo?.name} </div>
+              ) : (
+                <button className="btn login-btn" onClick={onClickLoginLogout}>
+                  Login/Signup
+                </button>
+              )}
+            </div>
+          ) : null}
         </div>
       </div>
     </header>
--- src/layouts/MainLayout.js
+++ src/layouts/MainLayout.js
@@ -1,44 +1,42 @@
 import React, { useEffect, useRef, useState } from "react";
-import { Navigate } from 'react-router-dom';
-
+import { Navigate } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import icons from "../common/Images";
+
 import Modal from "../components/Modal";
 
 import RingLoader from "../components/RingLoader";
-// import Breadcrumbs from "../components/Breadcrumb";
 import { withContext } from "../context/Context";
+
+// import Breadcrumbs from "../components/Breadcrumb";
 // import Arrow from "../../src/assets/arrow-left.svg";
 
-const { logo, home_main, location, home, cross, no_jobs } = icons;
 
 function MainLayout(props) {
-  const { children, authRequired=true, loading = true, footer = false, title = "Test", context: {isUserLogin}, loginPage=false} = props;
-  const [login, checkLogin] = useState(false);
+  const {
+    children,
+    authRequired = true,
+    loading = true,
+    footer = false,
+    title = "Test",
+    context: { isUserLogin },
+    loginPage = false,
+  } = props;
 
-  // useEffect(() => {checkLogin(isUserLogin)}, [isUserLogin]);
 
   useEffect(() => {
     document.title = title;
   }, [title]);
 
-  if(!authRequired && isUserLogin && loginPage){
+  if (!authRequired && isUserLogin && loginPage) {
     return <Navigate to="/" replace />;
   }
 
-  if(authRequired && !isUserLogin){
+  if (authRequired && !isUserLogin) {
     return <Navigate to="/login" replace />;
   }
 
-  // if(redirect && redirectUrl){
-  //   history.push("/shop");
-  //   return null;
-  // }
-
-  // console.log({scrollPos});
-
-  if (loading){
+  if (loading) {
     return (
       <div className="main-loader">
         <RingLoader mainLoader />
@@ -48,9 +46,8 @@ function MainLayout(props) {
 
   return (
     <main className="main-body">
-      <Header />
-      {children} {footer ? <Footer /> : null}{" "}
-      {/* <Modal/> */}
+      <Header isLoginPage={loginPage}/>
+      {children} {footer ? <Footer /> : null} {/* <Modal/> */}
     </main>
   );
 }
